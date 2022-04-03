6fad6aae879e5352a4ce8ce5e0700718da46455d
turguteyupcan <turguteyupcan@gmail.com>
2022-04-03 21:02:57 +0300

    Mekanikler infinite runnera gore degisti
    Character Movement Guncellemesi yapildi. Jump eklendi, hareket sonsuza dek ileri gidecek sekilde hareket guncellendi. Android platforma gecildi ve input ona gore guncellendi.

--- DungeonPillager/Assets/PlayerMovement.cs
+++ DungeonPillager/Assets/PlayerMovement.cs
@@ -5,35 +5,38 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
-    public Transform cam;
+    public float speed = 12f;
 
-    public float speed = 6f;
+    public float gravity = -9.81f;
+    public float jumpHeight=3f;
+    
 
-    public float turnSmoothTime = 0.1f;
-    float turnSmoothVelocity;
+    public Transform grouncheck;
+    public float groundDistance = 0.1f;
+    public LayerMask groundMask;
 
-    // Update is called once per frame - TPS Update
-    /*void Update()
-    {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-
-        if(direction.magnitude > 0.1f)
-        {
-            float targetAngle=Mathf.Atan2(direction.x,direction.z)*Mathf.Rad2Deg+cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity,turnSmoothTime);
-            transform.rotation= Quaternion.Euler(0f,angle,0f);
-
-            Vector3 moveDirection= Quaternion.Euler(0f,targetAngle,0f)*Vector3.forward;
-            controller.Move(moveDirection.normalized * speed*Time.deltaTime);
-        }
-    }*/
-
+    Vector3 velocity;
+    bool isGrounded;
+    
     private void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
+        isGrounded = Physics.CheckSphere(grouncheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float horizontal = 1;
         Vector3 move = transform.right * horizontal;
         controller.Move(move*speed*Time.deltaTime);
+
+        if (Input.GetMouseButton(0) && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y+=gravity*Time.deltaTime;
+        controller.Move(velocity*Time.deltaTime);
     }
 }
