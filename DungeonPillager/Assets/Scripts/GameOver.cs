942d7a3782958372bf3644b8e460b63566113236
turguteyupcan <turguteyupcan@gmail.com>
2022-05-25 23:22:01 +0300

    Eski Versiyona Donme

--- DungeonPillager/Assets/Scripts/GameOver.cs
+++ DungeonPillager/Assets/Scripts/GameOver.cs
@@ -4,23 +4,12 @@ public class GameOver : MonoBehaviour
 {
     public PlayerMovement playerMovement;
     public Animator animator;
-    public Transform tile;
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag != "TileMove")
-        {
-            SoundManagerScript.PlaySound("death");
-            playerMovement.enabled = false;
-            other.enabled = false;
-            animator.SetTrigger("Dead");
-        }
-        else
-        {
-            tile.position = other.transform.position + new Vector3(23.8f, 0, 0);
-            //tile[Random.Range(0, tile.Length)].position = other.transform.position + new Vector3(23.8f, 0, 0);
-            other.transform.position += new Vector3(11.9f, 0, 0);
-        }
-        
+        SoundManagerScript.PlaySound("death");
+        playerMovement.enabled = false;
+        other.enabled = false;
+        animator.SetTrigger("Dead");
         
     }
 }
