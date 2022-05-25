0159150eb3f5d5152ed7374039a7ab6280b1320c
turguteyupcan <turguteyupcan@gmail.com>
2022-05-25 22:05:26 +0300

    Ileriye Tile Ekleme

--- DungeonPillager/Assets/Scripts/GameOver.cs
+++ DungeonPillager/Assets/Scripts/GameOver.cs
@@ -4,12 +4,23 @@ public class GameOver : MonoBehaviour
 {
     public PlayerMovement playerMovement;
     public Animator animator;
+    public Transform tile;
     private void OnTriggerEnter(Collider other)
     {
-        SoundManagerScript.PlaySound("death");
-        playerMovement.enabled = false;
-        other.enabled = false;
-        animator.SetTrigger("Dead");
+        if(other.tag != "TileMove")
+        {
+            SoundManagerScript.PlaySound("death");
+            playerMovement.enabled = false;
+            other.enabled = false;
+            animator.SetTrigger("Dead");
+        }
+        else
+        {
+            tile.position = other.transform.position + new Vector3(23.8f, 0, 0);
+            //tile[Random.Range(0, tile.Length)].position = other.transform.position + new Vector3(23.8f, 0, 0);
+            other.transform.position += new Vector3(11.9f, 0, 0);
+        }
+        
         
     }
 }
